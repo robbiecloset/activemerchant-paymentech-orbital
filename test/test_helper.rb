33c6ffec8b194fab38a46e39a6de86fc3ec6c211
John Corrigan <john@mintdigital.com>
2009-10-15 17:30:42 -0400

    Initial commit to activemerchant-paymentech-orbital.

---
+++ test/test_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'activemerchant-paymentech-orbital'
+
+class Test::Unit::TestCase
+end
