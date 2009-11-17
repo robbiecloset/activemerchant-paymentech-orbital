b514045ad2e4a1d13008e49c6ef7c6859d66233f
John Corrigan <john@mintdigital.com>
2009-11-17 17:32:31 -0500

    - Renamed some of the classes and correctly nested everything.

--- lib/billing/paymentech_orbital/request/end_of_day_request.rb
+++ lib/billing/paymentech_orbital/request/end_of_day_request.rb
@@ -1,14 +1,22 @@
-class EndOfDayRequest < PaymentechOrbitalRequest
-  def request_type; "EndOfDay"; end
+module ActiveMerchant
+  module Billing
+    module PaymentOrbitalGateway
+      module Request
+        class EndOfDayRequest < ::Request::Base
+          def request_type; "EndOfDay"; end
 
-  private
-  def request_body(xml)
-    add_meta_info(xml)
-  end
+          private
+          def request_body(xml)
+            add_meta_info(xml)
+          end
 
-  def add_meta_info(xml)
-    xml.tag! "BIN", bin
-    xml.tag! "MerchantID", merchant_id
-    xml.tag! "TerminalID", terminal_id
+          def add_meta_info(xml)
+            xml.tag! "BIN", bin
+            xml.tag! "MerchantID", merchant_id
+            xml.tag! "TerminalID", terminal_id
+          end
+        end
+      end
+    end
   end
 end
--- lib/billing/paymentech_orbital/request/void_request.rb
+++ lib/billing/paymentech_orbital/request/void_request.rb
@@ -1,31 +1,39 @@
-class VoidRequest < PaymentechOrbitalRequest
-  attr_reader :tx_ref_num, :tx_ref_idx, :money
+module ActiveMerchant
+  module Billing
+    module PaymentechOrbitalGateway
+      module Request
+        class VoidRequest < ::Request::Base
+          attr_reader :tx_ref_num, :tx_ref_idx, :money
 
-  def initialize(tx_ref_num, tx_ref_idx, money=nil, options={})
-    @tx_ref_num = tx_ref_num
-    @tx_ref_idx = tx_ref_idx
-    @money = money
-    super(options)
-  end
+          def initialize(tx_ref_num, tx_ref_idx, money=nil, options={})
+            @tx_ref_num = tx_ref_num
+            @tx_ref_idx = tx_ref_idx
+            @money = money
+            super(options)
+          end
 
-  def request_type; "Reversal"; end
+          def request_type; "Reversal"; end
 
-  private
-  def request_body(xml)
-    add_transaction_info(xml)
-    xml.tag! "AdjustedAmt", money if money
-    add_meta_info(xml)
-  end
+          private
+          def request_body(xml)
+            add_transaction_info(xml)
+            xml.tag! "AdjustedAmt", money if money
+            add_meta_info(xml)
+          end
 
-  def add_transaction_info(xml)
-    xml.tag! "TxRefNum", tx_ref_num
-    xml.tag! "TxRefIdx", tx_ref_idx
-  end
+          def add_transaction_info(xml)
+            xml.tag! "TxRefNum", tx_ref_num
+            xml.tag! "TxRefIdx", tx_ref_idx
+          end
 
-  def add_meta_info(xml)
-    xml.tag! "OrderID", order_id
-    xml.tag! "BIN", bin
-    xml.tag! "MerchantID", merchant_id
-    xml.tag! "TerminalID", terminal_id
+          def add_meta_info(xml)
+            xml.tag! "OrderID", order_id
+            xml.tag! "BIN", bin
+            xml.tag! "MerchantID", merchant_id
+            xml.tag! "TerminalID", terminal_id
+          end
+        end
+      end
+    end
   end
 end
