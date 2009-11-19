28914d4b050edee4ea6c737054082dc10aefb2d4
John Corrigan <john@mintdigital.com>
2009-11-19 17:49:56 -0500

    - Added options factories for all requests.

--- test/factories.rb
+++ test/factories.rb
@@ -11,7 +11,7 @@ Factory.define :credit_card, {
   f.add_attribute :type, 'visa'
 end
 
-# Pseudo factories
+# Option factories
 Options.define(:billing_address, :defaults => {
   :zip => "12345",
   :address1 => "123 Happy Place",
@@ -28,7 +28,38 @@ Options.define(:gateway_auth, :defaults => {
   :bin         => "1", 
   :terminal_id => "1"
 })
+Options.define(:soft_descriptors, :defaults => {
+  :merchant_name => 'merchant',
+  :merchant_url => 'mmmerchant.com'
+})
 
+# Request options:
+Options.define(:request_options, :parent => :gateway_auth)
+Options.define(:end_of_day_options, :parent => :request_options)
+Options.define(:new_order_options, :parent => [
+  :request_options, :billing_address, :soft_descriptors
+], :defaults => {
+  :currency_code => "840",
+  :currency_exponent => "2",
+  :order_id => "1",
+  :recurring_frequency => "#{Date.today.strftime("%d")} * ?"
+})
+Options.define(:profile_management_options, :parent => [
+  :request_options, :billing_address
+], :defaults => {
+  :customer_ref_num => "123456"
+})
+Options.define(:void_options, :parent => :request_options, :defaults => {
+  :tx_ref_num => "1", 
+  :tx_ref_idx => "1",
+  :order_id => "1"
+})
+
+# Pseudo factories
 def gateway(options={})
   ActiveMerchant::Billing::PaymentechOrbital::Gateway.new(Options(:gateway_auth, options))
+end
+
+def new_order_request(options={})
+  ActiveMerchant::Billing::PaymentechOrbital::Request::NewOrder.new(Options(:new_order_options, options))
 end
